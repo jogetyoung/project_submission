6718409fcdf5381921c5322613a521112c25d7da
jogetyoung <youngjoeljj@gmail.com>
2025-03-27 09:37:46 +0800

    finalized

--- angular/src/app/payment/cancel.component.ts
+++ angular/src/app/payment/cancel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cancel',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './cancel.component.css'
 })
 export class CancelComponent {
+
+  private router = inject(Router);
+
+  goBack() {
+    // Navigate back to the premium subscription page
+    this.router.navigate(['/business/premium']);
+  }
+
+  tryAgain() {
+    // Return to previous page in history
+    window.history.back();
+
+  }
 
 }
